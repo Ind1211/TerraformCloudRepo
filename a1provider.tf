2e1b51b46ffb6edacd06461229bfdf9a4c100264
IndraSen <indrasen1211@gmail.com>
2024-04-26 03:57:38 +0000

    test

--- a1provider.tf
+++ a1provider.tf
@@ -24,10 +24,17 @@ terraform {
   }  
 }
 
-# Provider Block
 provider "azurerm" {
- features {}          
+  features {}
+  skip_provider_registration = "true"
+  # Connection to Azure
+  subscription_id = var.subscription_id
+  client_id = var.client_id
+  client_secret = var.client_secret
+  tenant_id = var.tenant_id
+ 
 }
+ 
 variable "client_id" {
   type = string
 }
@@ -43,13 +50,3 @@ variable "subscription_id" {
 variable "tenant_id" {
   type = string
 }
-provider "azurerm" {
-  features {}
-  skip_provider_registration = "true"
-  # Connection to Azure
-  subscription_id = var.subscription_id
-  client_id = var.client_id
-  client_secret = var.client_secret
-  tenant_id = var.tenant_id
- }
- 
